417b99a3cdac73392931ddc9b52909238b94ea43
Andy Roush <andyclan@gmail.com>
2024-07-30 17:41:25 -0700

    resend email working now

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,9 +26,9 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
+      from: "Portfolio Contact Form <onboarding@resend.dev>",
       to: "andyroush17@gmail.com",
-      subject: "Message from contact form",
+      subject: "Message from portfolio contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
         message: message,
